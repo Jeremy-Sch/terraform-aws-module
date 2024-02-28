0b75e4f56599a259fe9f19bc6881fec03ae89404
Jeremy-Sch <61931733+Jeremy-Sch@users.noreply.github.com>
2024-02-28 10:59:45 +0100

    First commit

---
+++ main.tf
@@ -0,0 +1,32 @@
+# SSH Public Key
+resource "aws_key_pair" "ssh_key" {
+  key_name   = "jeremy_ssh_key"
+  public_key = file("${var.aws_ssh_public_key_file_path}")
+}
+
+# Security Group
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH access to host"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0",]
+  }
+
+  vpc_id = var.aws_vpc_id
+}
+
+# Instance
+resource "aws_instance" "app_server" {
+  ami           = var.aws_ami
+  instance_type = var.aws_instance_type
+  key_name      = aws_key_pair.ssh_key.key_name
+  subnet_id     = var.aws_subnet_id
+
+  tags = {
+    Name = var.aws_instance_tag
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,52 @@
+# AWS Details
+variable "aws_region" {
+  type        = string
+  description = "AWS region"
+  default     = "eu-west-3"
+}
+
+variable "aws_profile" {
+  type        = string
+  description = "AWS profile name"
+  default     = "ema2024"
+}
+
+variable "aws_shared_credentials_files_path" {
+  type        = list(string)
+  description = "AWS credentials and other settings"
+  default = ["~/.aws/profile"]
+}
+
+variable "aws_ssh_public_key_file_path" {
+  type        = string
+  description = "Provide your SSH public key file path"
+  default = "~/.ssh/aws"
+}
+
+variable "aws_ami" {
+  type        = string
+  description = "AWS AMI"
+}
+
+variable "aws_vpc_id" {
+  type        = string
+  description = "AWS VPC ID"
+}
+
+variable "aws_subnet_id" {
+  type        = string
+  description = "AWS Subnet ID"
+}
+
+variable "aws_instance_type" {
+  type        = string
+  description = "AWS Instance Type"
+  default     = "t3.micro"
+}
+
+
+variable "aws_instance_tag" {
+  type        = string
+  description = "AWS Instance Tag"
+  default     = "Terraform"
+}
